3e76f583a1a819099c342ff34570ae2d73c62a08
Dillona25 <Dillyb00by49@gmail.com>
2025-01-31 22:15:50 -0600

    added structure

---
+++ strings/script.py
@@ -0,0 +1,6 @@
+# ESCAPE CHARACTERS #
+
+# Escape Characters allow us to invlude special characters in our strings without causing errors
+
+quote = "Pat Mahomes once said, \"lets go win\"."
+print(quote)
