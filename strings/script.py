859e67bd5b5e46bb03c1a95b7e4e23022dc302b4
Dillona25 <Dillyb00by49@gmail.com>
2025-01-31 22:26:26 -0600

    added notes

--- strings/script.py
+++ strings/script.py
@@ -2,5 +2,22 @@
 
 # Escape Characters allow us to invlude special characters in our strings without causing errors
 
-quote = "Pat Mahomes once said, \"lets go win\"."
-print(quote)
+# quote = "Pat Mahomes once said, \"lets go win\"."
+# print(quote)
+
+# Common Escape characters include:
+# \' - Represents a single quote
+# \" - Represents a double quote
+# \\ - Represents a slash itself
+# \n - A new line
+# \t - A tab indentation
+
+# quote = "This is some test \ntext that we are going to make quite \nlong so we can create a new line"
+
+# better_quote = """This is some test
+# text that we are going to make quite
+# long so we can create a new line"""
+
+# print(better_quote)
+
+# FORMATTED STRINGS #
